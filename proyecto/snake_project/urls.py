1ad191e95363b506b3625a205fc3f42b4ce173b2
jponsgar <jponsgar@gmail.com>
2024-07-10 11:32:03 +0200

    proyecto

--- proyecto/snake_project/urls.py
+++
@@ -1,4 +0,0 @@
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('snake/', views.start_snake_game, name='start_snake_game'),
-]
--- proyecto/snake_project/views.py
+++
@@ -1,6 +0,0 @@
-import os
-from django.http import HttpResponse
-
-def start_snake_game(request):
-    os.system('python3 snake_game.py')
-    return HttpResponse("Game Over!")
