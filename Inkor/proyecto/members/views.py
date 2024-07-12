ab08a4cb190faa3a3acd0a72658b231a5cfd513b
jponsgar <jponsgar@gmail.com>
2024-07-12 12:25:46 +0200

    Inkor project

--- Inkor/proyecto/members/views.py
+++ Inkor/proyecto/members/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def members(request):
-  
-  # html = """
-  # <h1>Hola, mundo</h1>
-  # <p>Esto es un HTML</p>
-  # """
-  # return HttpResponse(html) # admite texto o HTML: "<h1>Hello world!</h1>"
-  
-  # Devolvemos HTML por archivo:
+def index(request):
+  # Devolvemos el HTML de templates/index.html:
   return render(request, "index.html")
+
+def members(request):
+  # Devolvemos el HTML de templates/members/index.html:
+  return render(request, "members/index.html")
