d85cef685dc247320ef45408405b9900ebaaed10
jponsgar <jponsgar@gmail.com>
2024-07-23 10:08:07 +0200

    Cambios

--- Ejercicios/ejercicio_prueba/project/myapp/models.py
+++ Ejercicios/ejercicio_prueba/project/myapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Snake(models.Model):
     nombre = models.CharField(max_length=100)
-    puntos = models.IntegerField()
+    puntos = models.IntegerField(null=True)
 
     def __str__(self):
         return self.nombre
