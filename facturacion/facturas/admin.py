ce1658d3deb2aee6025a9cf0d9b17c0c0c6c59ca
jponsgar <jponsgar@gmail.com>
2024-09-16 17:48:55 +0200

    Se elimina proyecto facturacion

--- facturacion/facturas/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Cliente, Producto, Factura, ProductoFactura
-
-admin.site.register(Cliente)
-admin.site.register(Producto)
-admin.site.register(Factura)
-admin.site.register(ProductoFactura)
-
