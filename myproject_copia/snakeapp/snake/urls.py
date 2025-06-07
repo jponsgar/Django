5c3b3ee876ad0ab190c3e1e56248af9940ff9547
jponsgar <jponsgar@gmail.com>
2025-06-07 17:18:14 +0200

    Eliminar myproject_copia

--- myproject_copia/snakeapp/snake/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import IndexView, SaveScoreView, ScoreboardView
-
-urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
-    path('save-score/', SaveScoreView.as_view(), name='save_score'),
-    path('scoreboard/', ScoreboardView.as_view(), name='scoreboard'),
-]
-
